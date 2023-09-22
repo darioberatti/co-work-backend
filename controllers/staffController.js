e0927c15d633cdff17ad7a0c5bcab1bb0772bf7b
Agustin Torroja <atorroja18@gmail.com>
2023-09-22 12:39:27 -0300

    mailer config & correction on cors origin route

--- controllers/staffController.js
+++ controllers/staffController.js
@@ -1,5 +1,6 @@
 const { Staff } = require("../service/staffServices");
 const { generateToken, validateToken } = require("../config/tokens");
+const transporter = require("../config/mailer")
 
 exports.listUsers = async (req, res) => {
   try {
@@ -29,8 +30,15 @@ exports.addUser = async (req, res) => {
     const registerToken = generateToken(payload);
 
     //Acá debería ir la lógica para mandar el token por email.
-    console.log(registerToken);
 
+    transporter.sendMail({
+            from: '"Retro Futbol Club" <e.retrofutbolclub@gmail.com>', // sender address
+            to: payload.email, // list of receivers
+            subject: "Confirmacion de registro de usuario", // Subject line
+            html: `<h2>Hola ${payload.email}! Tu usuario se registro con éxito</h2>
+            <a href=http://localhost:3000/confirm-user/${registerToken}>Redirigir al sitio:</a>
+            <h4><b>Muchisimas gracias!</b></h4>`,
+          });
     res.send(result);
   } catch (error) {
     res.status(400).send(error);
