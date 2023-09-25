3772afa44f466175c85d939d731817122d40ecb4
Dario Beratti <darioberatti@gmail.com>
2023-09-25 11:07:39 -0300

    Server health check configuration. Environment variables set

--- config/db/db.js
+++ config/db/db.js
@@ -2,24 +2,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(
-  "cowork",
-  process.env.USER || "cowork_user",
-  process.env.PASSWORD || "XfOiCVayOLvjY6Jn9Xyr4yryVabARtyg",
-  {
-    host: process.env.HOST || "dpg-ck6aa3vq54js73a66jkg-a",
-    dialect: "postgres",
-    logging: false,
-  }
-);
-
-/* const db = new Sequelize("cowork", null, null, {
-  host: "localhost",
+const db = new Sequelize("cowork", process.env.USER, process.env.PASSWORD, {
+  host: process.env.HOST,
   dialect: "postgres",
   logging: false,
-  "dpg-ck6aa3vq54js73a66jkg-a"
-  "cowork_user"
-  "XfOiCVayOLvjY6Jn9Xyr4yryVabARtyg"
-}); */
+});
 
 module.exports = db;
