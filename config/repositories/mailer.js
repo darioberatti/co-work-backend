2f978c9864fa3d4a63a3e299118a2731d1718394
Agustin Torroja <atorroja18@gmail.com>
2023-09-26 10:52:40 -0300

    bugfix on mailer, correction on export fn sendEmail & staffController for addUser

--- config/repositories/mailer.js
+++ config/repositories/mailer.js
@@ -1,4 +1,6 @@
 const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
@@ -10,17 +12,20 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+const originUrl = process.env.ORIGIN;
+
+
 transporter.verify().then(() => console.log("Ready to send email"));
 
-exports.sendEmail = (to, registerToken) => {
+const sendEmail = (to, registerToken) => {
   transporter.sendMail({
     from: '"Retro Futbol Club" <e.retrofutbolclub@gmail.com>', // sender address
     to: to, // list of receivers
     subject: "Confirmacion de registro de usuario", //Subject line
     html: `<h2>Hola ${to}! Tu usuario se registro con éxito</h2>
-    <a href=http://localhost:3000/confirm-user/${registerToken}>Redirigir al sitio:</a>
+    <a href=${originUrl}/confirm-user/${registerToken}>Redirigir al sitio:</a>
     <h4><b>Muchisimas gracias!</b></h4>`,
   });
 };
 
-module.exports = transporter;
+module.exports = {transporter, sendEmail};
