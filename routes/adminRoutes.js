c64d5aaa4f4b4a302e99a366bdeb8dd626dfc4fc
GRabinovich <garabinovich@gmail.com>
2023-09-29 11:20:51 -0300

    move controllers services

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { listOffices, getOfficeById, addOffice } = require("../controllers/adminController");
+const { listOffices, getOfficeById, addOffice, editOffice } = require("../controllers/adminController");
 
 // Ruta para listar todas las oficinas.
 router.get("/offices", listOffices);
@@ -10,5 +10,8 @@ router.get("/offices/:officeId", getOfficeById);
 
 //Ruta para agregar una nueva oficina.
 router.post("/offices", addOffice)
+
+//Ruta para editar cualquier campo de la oficina.
+router.put("/offices/:officeId", editOffice)
 
 module.exports = router;
--- service/adminServices.js
+++ service/adminServices.js
@@ -1,5 +1,6 @@
 const Offices = require("../models/Offices");
-const Floors = require("../models/Floors")
+const Floors = require("../models/Floors");
+const Tables = require("../models/Tables");
 
 class Admin {
   static async showAll() {
@@ -11,7 +12,7 @@ class Admin {
       },
     });
   }
-  
+
   static async showByPk(officeId) {
     return Offices.findByPk(officeId, {
       include: {
@@ -46,6 +47,46 @@ class Admin {
       phoneNumber,
       urlImg,
     });
+  }
+
+  static async edit(officeId, officeData) {
+    try {
+      const office = await Offices.findByPk(officeId);
+
+      if (!office) throw new Error("Usuario no encontrado");
+
+      const result = await office.update(officeData, { returning: true });
+
+      return result;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  static async relation(result, floorsNumber) {
+    const floorsPromises = [];
+    for (let i = 1; i <= floorsNumber; i++) {
+      const floor = await Floors.create({
+        number: i,
+        tablesNumber: 1,
+      });
+      const tablePromises = [];
+      for (let j = 1; j <= floor.tablesNumber; j++) {
+        tablePromises.push(
+          Tables.create({
+            name: `Floor ${i} - Table A`,
+            floor: floor.number,
+            capacity: 6,
+          })
+        );
+      }
+      const createdTables = await Promise.all(tablePromises);
+      await floor.addTables(createdTables);
+      await result.addFloors(floor);
+      floorsPromises.push(floor);
+    }
+
+    await result.addFloors(floorsPromises);
   }
 }
 
