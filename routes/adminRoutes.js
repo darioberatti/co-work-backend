9aa8dd0d31bccb9070ebaadec586adc980854e08
GRabinovich <garabinovich@gmail.com>
2023-10-02 18:06:34 -0300

    add controller editTableCapacity

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { listOffices, getOfficeById, addOffice, editOffice } = require("../controllers/adminController");
+const { listOffices, getOfficeById, addOffice, editOffice, editTableCapacity } = require("../controllers/adminController");
 
 // Ruta para listar todas las oficinas.
 router.get("/offices", listOffices);
@@ -13,5 +13,8 @@ router.post("/offices", addOffice)
 
 //Ruta para editar cualquier campo de la oficina.
 router.put("/offices/:officeId", editOffice)
+
+// Ruta para editar la capacidad de una mesa.
+router.put("/offices/:officeId/tables/:tableId", editTableCapacity);
 
 module.exports = router;
