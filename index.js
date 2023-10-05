5c8bb64f564bf8e98dc39ed0d32da2152bbbd91c
Dario Beratti <darioberatti@gmail.com>
2023-10-05 14:42:50 -0300

    DB force:false

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use((err, req, res, next) => {
   res.status(500).send(err.message);
 });
 
-db.sync({ force: true })
+db.sync({ force: false })
   .then(() => {
     console.log("DB Connected");
     app.listen(port, () => console.log(`Servidor escuchando ${port}`));
